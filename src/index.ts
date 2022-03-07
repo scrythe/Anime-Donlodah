8040848a2b78bcfb8ee4388fd7d75e49b5beeca3
Magomed <scrythepvp@gmail.com>
2022-03-07 16:27:03 +0100

    feat: add multiple links save in array plus error handeling
    links have now better error handeling, work with multiple links
    listed on a variable and all links are saved in an array with
    Promise.all (multiple links executed parallel, better than with for
    or other loops...)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as cheerio from 'cheerio';
 
 let animeEpisodes: string[] = [
   'https://anicloud.io/anime/stream/toradora/staffel-1/episode-8',
+  'https://anicloud.io',
   'https://anicloud.io/anime/stream/toradora/staffel-1/episode-19',
   'https://anicloud.io/anime/stream/toradora/staffel-1/episode-20',
   'https://anicloud.io/anime/stream/toradora/staffel-1/episode-21',
@@ -13,27 +14,48 @@ let animeEpisodes: string[] = [
 function getStreamLinks(url: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     let mainUrl = new URL(url);
-    const res = await axios.get(url);
+    const res = await axios.get(url).catch((error) => {
+      throw Error(error);
+      // reject();
+    });
     const $ = cheerio.load(res.data);
     let linkElement = $(
       '.hosterSiteVideo > ul > li > div > a > h4:contains(Streamtape)'
     )
       .parent()
       .attr('href');
-    if (!linkElement) {
+    if (linkElement) {
+      mainUrl.pathname = linkElement;
+      resolve(mainUrl.href);
+    } else {
       reject(`Element ${linkElement} not found on site ${url}`);
     }
-    mainUrl.pathname = linkElement ?? 'undefined';
-    resolve(mainUrl.href);
   });
 }
 
-(() => {
-  getStreamLinks(
-    'https://anicloud.io/anime/stream/toradora/staffel-1/episode-8'
-  )
-    .then((link) => console.log(link))
-    .catch((err) => console.log(err));
+(async () => {
+  let animeStreamLinksPromises = animeEpisodes.map((link) =>
+    getStreamLinks(link).catch((error) => console.error(error))
+  );
+  let animeStreamLinks = (await Promise.all(animeStreamLinksPromises)).filter(
+    (link): link is string => {
+      return !!link;
+    }
+  );
+  console.log(animeStreamLinks);
+
+  /* let animeStreamLinks = await animeEpisodes.map(async (link) => {
+    let test = await getStreamLinks(link);
+    return await getStreamLinks(link);
+  });
+  console.log(animeStreamLinks); */
+  /* animeEpisodes.forEach((link) => {
+    getStreamLinks(
+      'https://anicloud.io/anime/stream/toradora/staffel-1/episode-800'
+    )
+      .then((link) => console.log(link))
+      .catch((err) => console.log(err));
+  }); */
 })();
 
 /* (async () => {
