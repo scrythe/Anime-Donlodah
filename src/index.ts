53f6471bcda211ad5f74ba52b16be9837daa7e97
Magomed <scrythepvp@gmail.com>
2022-03-08 22:59:01 +0100

    feat: get download link and console.logs it in the terminal
    links are being gotten from the 9xbuddy site and then the backup
    download link is being console.logged

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ import { Browser } from 'puppeteer';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 let animeEpisodes: string[] = [
-  'https://anicloud.io/anime/stream/toradora/staffel-1/episode-8',
+  // 'https://anicloud.io/anime/stream/toradora/staffel-1/episode-8',
   'https://anicloud.io/anime/stream/toradora/staffel-1/episode-19',
   'https://anicloud.io/anime/stream/toradora/staffel-1/episode-20',
-  'https://anicloud.io/anime/stream/toradora/staffel-1/episode-21',
-  'https://anicloud.io/anime/stream/toradora/staffel-1/episode-22',
+  // 'https://anicloud.io/anime/stream/toradora/staffel-1/episode-21',
+  // 'https://anicloud.io/anime/stream/toradora/staffel-1/episode-22',
 ];
 
 function getStreamLinks(url: string): Promise<string> {
@@ -34,6 +34,47 @@ function getStreamLinks(url: string): Promise<string> {
   });
 }
 
+function goToUrl(browser: Browser, url: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    let voeUrl = new URL('https://voe.sx/');
+    const page = await browser.newPage();
+    await page.goto(url);
+    let puppeteerUrl = new URL(page.url());
+    if (puppeteerUrl.hostname != voeUrl.hostname) {
+      await page.reload();
+    }
+    // recaptcha challenge expires in two minutes
+    await page.waitForNavigation().catch(async (error) => {
+      console.error(error);
+      await page.reload();
+      await page.waitForNavigation();
+    });
+    puppeteerUrl.href = page.url();
+    if (puppeteerUrl.hostname == voeUrl.hostname) {
+      let voeUrlLink = page.url();
+      page.close();
+      resolve(voeUrlLink);
+    } else {
+      reject(`Page: ${page.url()} is not a VOE url`);
+    }
+  });
+}
+
+async function getDownloadLink(browser: Browser, url: string) {
+  let xbudUrl = new URL('https://9xbud.com/');
+  xbudUrl.pathname = url;
+  let page = await browser.newPage();
+  await page.goto(xbudUrl.href);
+  // await page.waitForNavigation();
+  let downloadLinkXpath =
+    "//a/span[contains(text(), 'Download Now')]/parent::a";
+  await page.waitForXPath(downloadLinkXpath);
+  let downloadLinks = await page.$x(downloadLinkXpath);
+  let getdownloadLinkJson = await downloadLinks[1].getProperty('href');
+  let getDownloadLink: string = await getdownloadLinkJson.jsonValue();
+  console.log(getDownloadLink);
+}
+
 (async () => {
   let animeStreamLinksPromises = animeEpisodes.map((link) =>
     getStreamLinks(link).catch((error) => console.error(error))
@@ -54,30 +95,6 @@ function getStreamLinks(url: string): Promise<string> {
       return !!url;
     }
   );
-  browser.close();
-  console.log(voeUrls);
+  voeUrls.forEach((url) => getDownloadLink(browser, url));
+  // getDownloadLink(browser, 'https://voe.sx/e/7ssj4bw9djeg');
 })();
-
-function goToUrl(browser: Browser, url: string): Promise<string> {
-  return new Promise(async (resolve, reject) => {
-    let voeUrl = new URL('https://voe.sx/');
-    const page = await browser.newPage();
-    await page.goto(url);
-    let puppeteerUrl = new URL(page.url());
-    if (puppeteerUrl.hostname != voeUrl.hostname) {
-      await page.reload();
-    }
-    // recaptcha challenge expires in two minutes
-    await page.waitForNavigation().catch(async (error) => {
-      console.error(error);
-      await page.reload();
-      await page.waitForNavigation();
-    });
-    puppeteerUrl.href = page.url();
-    if (puppeteerUrl.hostname == voeUrl.hostname) {
-      resolve(page.url());
-    } else {
-      reject(`Page: ${page.url()} is not a VOE url`);
-    }
-  });
-}
