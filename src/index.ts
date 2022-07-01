ea4d90b47d80f985ccc24a1265090e0f1ea81da2
Magomed <scrythepvp@gmail.com>
2022-07-01 19:52:20 +0200

    feat!: download video
    BREAKING_CHANGE: way faster download speeds than before

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import episodes from './episode-list.json';
 import puppeteer from 'puppeteer';
 import { getVoeLink } from './aniworldScraper';
 import { getM3u8Link } from './voeScraper';
+import { hlsDownload } from './utils';
 
 async function downloadVideos(episode: string) {
   const browser = await puppeteer.launch({ headless: false });
   const voeLink = await getVoeLink(browser, episode);
   const m3u8Link = await getM3u8Link(voeLink);
-  console.log(m3u8Link);
-  // download videos
+  hlsDownload(m3u8Link, 'downloads/video.mp4');
   browser.close();
 }
 
