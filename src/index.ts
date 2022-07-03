0a582c9107b48c643008b919f4462652bda29ac3
Magomed <scrythepvp@gmail.com>
2022-07-03 23:14:22 +0200

    feat: select all episodes between start and end

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { prompt } from 'enquirer';
 import animeList from './anime-list.json';
 import { getAllEpisodes } from './animeScraper';
-import { AllEpisodes } from './interfaces';
+import { AllEpisodes, Episode } from './interfaces';
 
 const animeListNames = animeList.map((animeEl) => animeEl.name);
 
@@ -29,6 +29,60 @@ interface EpisodePrompt {
   selectedEpisode: string;
 }
 
+async function selectEpisode(seasons: AllEpisodes, startOrEnd: string) {
+  const seasonNames = seasons.map((season) => season.name);
+
+  const { selectedSeason } = await prompt<SeasonPrompt>({
+    type: 'autocomplete',
+    name: 'selectedSeason',
+    message: `Select Start and End of Episodes \n Select ${startOrEnd} Season`,
+    //@ts-ignore
+    limit: 10,
+    choices: seasonNames,
+  });
+
+  const episodesOfSeason = getEpisodesOfSeason(selectedSeason, seasons);
+  if (!episodesOfSeason) return;
+  const episodeNames = episodesOfSeason.map((episode) => episode.name);
+
+  const { selectedEpisode } = await prompt<EpisodePrompt>({
+    type: 'autocomplete',
+    name: 'selectedEpisode',
+    message: `Select Start and End of Episodes \n Select ${startOrEnd} Episode`,
+    //@ts-ignore
+    limit: 10,
+    choices: episodeNames,
+  });
+
+  return selectedEpisode;
+}
+
+function selectEpisodesBetween(
+  startEpisodeName: string,
+  endEpisodeName: string,
+  allEpisodes: Episode[]
+) {
+  const startEpisode = allEpisodes.find(
+    (episode) => episode.name == startEpisodeName
+  );
+  const endEpisode = allEpisodes.find(
+    (episode) => episode.name == endEpisodeName
+  );
+
+  if (!startEpisode) return;
+  if (!endEpisode) return;
+
+  const startEpisodeIndex = allEpisodes.indexOf(startEpisode);
+  const endEpisodeIndex = allEpisodes.indexOf(endEpisode);
+
+  const selectedEpisodes = allEpisodes.slice(
+    startEpisodeIndex,
+    endEpisodeIndex + 1
+  );
+
+  return selectedEpisodes;
+}
+
 async function main() {
   const { selectedAnime } = await prompt<AnimeListPrompt>({
     type: 'autocomplete',
@@ -42,31 +96,37 @@ async function main() {
   const animeUrl = getAnimeUrl(selectedAnime);
   if (!animeUrl) return;
   const seasons = await getAllEpisodes(animeUrl);
-  const seasonNames = seasons.map((season) => season.name);
 
-  const { selectedSeason } = await prompt<SeasonPrompt>({
-    type: 'autocomplete',
-    name: 'selectedSeason',
-    message: 'Select Start and End of Episodes \n Select Start Season',
-    //@ts-ignore
-    limit: 10,
-    choices: seasonNames,
-  });
+  const episodeStart = await selectEpisode(seasons, 'start');
+  const episodeEnd = await selectEpisode(seasons, 'end');
 
-  const episodesOfSeason = getEpisodesOfSeason(selectedSeason, seasons);
-  if (!episodesOfSeason) return;
-  const episodeNames = episodesOfSeason.map((episode) => episode.name);
+  const allEpisodes = seasons.map((season) => season.episodes).flat();
+  if (!episodeStart) return;
+  if (!episodeEnd) return;
 
-  const { selectedEpisode } = await prompt<EpisodePrompt>({
-    type: 'autocomplete',
-    name: 'selectedEpisode',
-    message: 'Select Start and End of Episodes \n Select Start Episode',
-    //@ts-ignore
-    limit: 10,
-    choices: episodeNames,
-  });
+  const selectedEpisodes = selectEpisodesBetween(
+    episodeStart,
+    episodeEnd,
+    allEpisodes
+  );
 
-  console.log(selectedEpisode);
+  console.log(selectedEpisodes);
+}
+
+async function selectedEpisodesTest() {
+  const animeUrl = getAnimeUrl('Hunter x Hunter');
+  if (!animeUrl) return;
+  const seasons = await getAllEpisodes(animeUrl);
+  const episodeStart = 'Departure x And x Friends [Episode 001]';
+  const episodeEnd = 'Strengthen x And x Threaten [Episode 064]';
+  const allEpisodes = seasons.map((season) => season.episodes).flat();
+  const selectedEpisodes = selectEpisodesBetween(
+    episodeStart,
+    episodeEnd,
+    allEpisodes
+  );
+  console.log(selectedEpisodes);
 }
 
 main();
+selectedEpisodesTest;
