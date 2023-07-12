1ad24f7d759c5de2efaca9fb20db5d0c6a5c187c
Magomed <scrythepvp@gmail.com>
2023-07-12 20:14:03 +0200

    feat: add template 9anime scraper
    added 9anime scraper, can only show retrieve hardcoded list of animes
    yet, will be on website.

---
+++ 9animeScraper/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(bodyParser.json());
+
+app.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080/');
+});
+
+app.get('/animeList', async (_req, res) => {
+  const animes = await prisma.anime.findMany();
+  res.status(200).json({ animes });
+});
