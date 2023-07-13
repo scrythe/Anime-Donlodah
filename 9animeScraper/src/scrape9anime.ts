a1afdfb605ce65e662bc06a1ccab53e06838540c
Magomed <scrythepvp@gmail.com>
2023-07-13 14:57:14 +0200

    feat: add method to get all animes with puppeteer
    get all animes using puppeteer instead of axios. This reduces bot
    detection and also once bot detection is triggered, easier to handle
    (not yet tho).

--- 9animeScraper/src/scrape9anime.ts
+++ 9animeScraper/src/scrape9anime.ts
@@ -1,62 +1,109 @@
-import axios from 'axios';
-import { JSDOM } from 'jsdom';
+import { Browser, Page } from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import randomUseragent from 'random-useragent';
 
 interface Anime {
   name: string;
   link: string;
 }
 
-interface AnimeFromPage {
-  page: number;
-  animes: Anime[];
+async function createPuppeteerBrowser(): Promise<Browser> {
+  puppeteer.use(StealthPlugin());
+  const browser: Browser = await puppeteer.launch({ headless: false });
+  return browser;
 }
 
-async function getDomOfPage(url: string): Promise<Document> {
-  const res = await axios.get(url, {
-    headers: {
-      'User-Agent': 'private',
-      Cookie: 'private',
-    },
+async function createNewPage(browser: Browser): Promise<Page> {
+  //Randomize User agent or Set a valid one
+  const userAgent = randomUseragent.getRandom();
+  const page = await browser.newPage();
+  await page.setUserAgent(userAgent);
+
+  //Randomize viewport size
+  await page.setViewport({
+    width: 1920 + Math.floor(Math.random() * 100),
+    height: 3000 + Math.floor(Math.random() * 100),
+    deviceScaleFactor: 1,
+    hasTouch: false,
+    isLandscape: false,
+    isMobile: false,
   });
-  const dom = new JSDOM(res.data);
-  return dom.window.document;
+
+  //Skip images/styles/fonts loading for performance
+  await page.setRequestInterception(true);
+  page.on('request', (req) => {
+    if (
+      req.resourceType() == 'stylesheet' ||
+      req.resourceType() == 'font' ||
+      req.resourceType() == 'image'
+    ) {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+  return page;
+}
+
+async function makePageWaitRandomTime(page: Page) {
+  await page.waitForTimeout((Math.floor(Math.random() * 12) + 5) * 1000);
+}
+
+async function selectorNotFound(page: Page) {
+  await page.setRequestInterception(false);
+  console.log('Selector not found');
 }
 
 async function getAnimeList() {
-  const animesAmount = await getAnimesAmount();
-  const allAnimes = await getAllAnimes(animesAmount);
+  const browser = await createPuppeteerBrowser();
+  const page = await createNewPage(browser);
+  const animesAmount = await getAnimesAmount(page);
+  console.log(animesAmount);
+  const allAnimes = await getAllAnimes(page, animesAmount);
   console.log(allAnimes);
+  browser.close();
+  return;
 }
 
-async function getAnimesAmount(): Promise<number> {
-  const document = await getDomOfPage('https://9anime.to/az-list');
-  const lastPageLink = document.querySelector<HTMLAnchorElement>(
-    'a.page-link[rel=last]'
-  );
-  const lastPageNumberStr = lastPageLink.href.split('page=')[1];
+async function getAnimesAmount(page: Page): Promise<number> {
+  page.goto('https://9anime.to/az-list');
+  const lastPageLinkEl = await page.waitForSelector('a.page-link[rel=last]');
+  const lastPageLink = await lastPageLinkEl.evaluate((el) => el.href);
+  const lastPageNumberStr = lastPageLink.split('page=')[1];
   return parseInt(lastPageNumberStr);
 }
 
-function getAllAnimes(animesAmount: number) {
-  const promises: Promise<AnimeFromPage>[] = [];
-  for (let page = 1; page <= animesAmount; page++) {
-    promises.push(getAnimesFromPage(page));
+async function getAllAnimes(
+  page: Page,
+  animesAmount: number
+): Promise<Anime[]> {
+  const animes: Anime[] = [];
+  for (let pageNumber = 1; pageNumber <= animesAmount; pageNumber++) {
+    const animesFromPage = await getAnimesFromPage(page, pageNumber);
+    animes.push(...animesFromPage);
   }
-  // return Promise.all(promises);
+  return animes;
 }
 
-async function getAnimesFromPage(page: number): Promise<AnimeFromPage> {
-  const document = await getDomOfPage(`https://9anime.to/az-list?page=${page}`);
-  const animeItems = document.querySelectorAll<HTMLAnchorElement>(
-    'div#list-items div.item .name.d-title'
-  );
-  const animes: Anime[] = Array.from(animeItems).map((animeItem) => {
-    const name = animeItem.textContent;
-    const link = animeItem.href;
-    return { name, link };
+async function getAnimesFromPage(
+  page: Page,
+  pageNumber: number
+): Promise<Anime[]> {
+  await page.goto(`https://9anime.to/az-list?page=${pageNumber}`);
+  await page.waitForSelector('div#list-items');
+  const animes = await page.evaluate(() => {
+    const animeItems = document.querySelectorAll<HTMLAnchorElement>(
+      'div#list-items div.item a.name.d-title'
+    );
+    const animes: Anime[] = Array.from(animeItems).map((animeItem) => {
+      const name = animeItem.textContent;
+      const link = animeItem.href;
+      return { name, link };
+    });
+    return animes;
   });
-  console.log(animes);
-  return { page, animes };
+  return animes;
 }
 
 getAnimeList();
